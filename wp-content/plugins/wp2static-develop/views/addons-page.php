5f868daf8c98ba3f411faf5c897060520deec69b
Kavan Patel <109383487+KAVAN-PAT3L@users.noreply.github.com>
2022-07-16 14:48:35 +0530

    First Try

---
+++ wp-content/plugins/wp2static-develop/views/addons-page.php
@@ -0,0 +1,24 @@
+<div class="wrap">
+    <br>
+
+    <table class="widefat striped">
+        <thead>
+            <tr>
+                <th>Enabled</th>
+                <th>Name</th>
+                <th>Type</th>
+                <th>Documentation URL</th>
+                <th>Configure</th>
+            </tr>
+        </thead>
+        <tbody>
+                            <tr>
+                    <td colspan="4">No addons are installed. <a href="https://wp2static.com/download">Get Add-Ons</a></td>
+                </tr>
+            
+
+                    </tbody>
+    </table>
+
+    <br>
+</div>
---
+++ wp-content/plugins/wp2static-develop/views/logs-page.php
@@ -0,0 +1,22 @@
+<div class="wrap">
+    <br>
+
+    <table class="widefat striped">
+        <thead>
+            <tr>
+                <th>When</th>
+                <th>What</th>
+            </tr>
+        </thead>
+        <tbody>
+                            <tr>
+                    <td colspan="2">Logs are empty.</td>
+                </tr>
+            
+
+                    </tbody>
+    </table>
+
+    <br> 
+
+    </div>
